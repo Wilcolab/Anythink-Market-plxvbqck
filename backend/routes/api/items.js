259adff1ebb2820b0c644f690bc7e47fbe499aa1
jss118 <103734365+jss118@users.noreply.github.com>
2023-01-03 13:26:09 +0000

    implement query api by title

--- backend/routes/api/items.js
+++ backend/routes/api/items.js
@@ -54,6 +54,7 @@ router.get("/", auth.optional, function(req, res, next) {
   }
 
   if (typeof req.query.title !== 'undefined') {
+    
     const regex = new RegExp(req.query.title, 'i')
 
     query.title = { $regex: regex}
